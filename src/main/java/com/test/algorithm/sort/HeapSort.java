26347cce1a65dbb0ec2c80ca9aae7119c9ed5a2c
superpangdudu <1659929144@qq.com>
2020-09-02 14:32:50 +0800

    Initial Heap sort commision

---
+++ src/main/java/com/test/algorithm/sort/HeapSort.java
@@ -0,0 +1,31 @@
+package com.test.algorithm.sort;
+
+/**
+ * HeapSort, a O(nlgn) sorting method
+ *
+ *                           1
+ *                          16
+ *                         /  \
+ *                        2    3
+ *                       14    10
+ *                      / \    / \
+ *                     4   5  6   7
+ *                     8   7  9   3
+ *                    / \ /
+ *                   8  9 10
+ *                   2  4 1
+ *
+ * Index:   1  2  3  4  5  6  7  8  9  10
+ * Value:   16 14 10 8  7  9  3  2  4  1
+ *
+ * Parent(i) = i / 2
+ * Left(i)   = 2i
+ * Right(i)  = 2i + 1
+ */
+public class HeapSort {
+
+    //=========================================================================
+    public static void main(String[] args) {
+
+    }
+}
