b24601887f170b072a848f33374e47fd5a6538c9
superpangdudu <1659929144@qq.com>
2020-09-02 16:10:09 +0800

    Heap sort implementation

--- src/main/java/com/test/algorithm/sort/HeapSort.java
+++ src/main/java/com/test/algorithm/sort/HeapSort.java
@@ -24,8 +24,41 @@ package com.test.algorithm.sort;
  */
 public class HeapSort {
 
+    public void maxHeapify(int[] A, int i) {
+        int l = 2 * (i + 1) - 1;
+        int r = 2 * (i + 1);
+        int largest = i;
+
+        //
+        if (l < A.length && A[l] > A[i])
+            largest = l;
+
+        //
+        if (r < A.length && A[r] > A[largest])
+            largest = r;
+
+        //
+        if (largest != i) {
+            int tmp = A[i];
+            A[i] = A[largest];
+            A[largest] = tmp;
+
+            //
+            maxHeapify(A, largest);
+        }
+    }
+
+    public void buildMaxHeap(int[] A) {
+        for (int i = A.length / 2; i >= 0; i--) {
+            maxHeapify(A, i);
+        }
+    }
+
     //=========================================================================
     public static void main(String[] args) {
-
+        HeapSort obj = new HeapSort();
+        int[] data = new int[] {1, 4, 3, 14, 16, 10, 8, 7, 9, 2};
+        obj.buildMaxHeap(data);
+        int n = 0;
     }
 }
